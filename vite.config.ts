18b99eee784bbba3ba7c21620ec5b71023a72f2c
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2023-08-27 22:20:02 +0700

    add code splitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,28 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react(), dts(), splitVendorChunkPlugin()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'React Barcode QrCode ',
       formats: ['es'],
       fileName: 'react-barcode-qrcode',
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const segments = id.split('/');
+            const dir = segments[segments.lastIndexOf('node_modules') + 1];
+            return `vendor-${dir}`;
+          }
+        },
+      },
     },
   },
 });
